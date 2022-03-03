1c2e9c2911bb2d4e9b7873342902b697c624967f
temodi <tibor.emodi@gmail.com>
2022-03-03 19:36:51 +0100

    String props

--- 20220303_js_react_props/App.js
+++ 20220303_js_react_props/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import BasicProps from './components/BasicProps';
 import BooleanProps from './components/BooleanProps';
 import NumericProps from './components/NumericProps';
-
+import StringProps from './components/StringProps';
 
 function App() {
 
   const logikaiVar = true;
   const numericVar = 3123;
+  const strVar = "Helloka";
 
   const generateNumber = () => 42;
 
@@ -16,12 +17,18 @@ function App() {
 
   return (
     <>
-     <BasicProps content="hellok" />
-     <BooleanProps logikaiTrue={ true } logikaiFalse={ false } logikaiVar={ logikaiVar } />
+      <BasicProps content="hellok" />
+      <BooleanProps logikaiTrue={ true } logikaiFalse={ false } logikaiVar={ logikaiVar } />
 
-     <NumericProps number={ 231 } numberVar={ numericVar } numberFuncReturn={ generateNumber() } />
+      <NumericProps number={ 231 } numberVar={ numericVar } numberFuncReturn={ generateNumber() } />
     
-
+      <StringProps
+        str1="Szoveg 1"
+        str2={ "Szoveg 2" }
+        str3={ strVar }
+        str4={ `String literal: ${strVar} `}
+        className="bg-red"
+      />
     </>     
   );
 }
---
+++ 20220303_js_react_props/components/StringProps.js
@@ -0,0 +1,14 @@
+
+
+function StringProps(props) {
+  return (
+    <div className={ props.className }>
+      str1: { props.str1 }<br />
+      str2: { props.str2 }<br />
+      str3: { props.str3 }<br />
+      str4: { props.str4 }<br />
+    </div>
+  )
+}
+
+export default StringProps;
